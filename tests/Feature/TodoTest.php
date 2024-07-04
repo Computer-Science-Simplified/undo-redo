8db972d700d92c0a5e628c75893223252747016e
martinjoo <martin@martinjoo.dev>
2024-07-04 16:15:12 +0200

    Refactor tests

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Redis;
 use PHPUnit\Framework\Attributes\Test;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
@@ -12,6 +13,13 @@ class TodoTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected function tearDown(): void
+    {
+        Redis::flushall();
+
+        parent::tearDown();
+    }
+
     #[Test]
     public function it_should_undo_and_redo_description_changes()
     {
@@ -19,32 +27,31 @@ class TodoTest extends TestCase
 
         $this->actingAs($user);
 
-        $todo = $this->postJson(route('todos.store'), [
-            'title' => 'Test Todo',
-        ])->json('data');
+        $todo = $this->createTodo('Test Todo');
 
-        $this->patchJson(route('todos.update-description', ['todo' => $todo['id']]), [
-            'description' => 'Test description',
-        ]);
+        $this->updateDescription($todo, 'Test description');
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->json('data');
+        $todo = $this->getTodo($todo);
 
         $this->assertSame('Test description', $todo['description']);
 
-        $this->patchJson(route('todos.undo', ['todoId' => $todo['id']]));
+        $this->undo($todo);
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->json('data');
+        $todo = $this->getTodo($todo);
 
         $this->assertNull($todo['description']);
 
-        $this->patchJson(route('todos.redo', ['todoId' => $todo['id']]));
+        $this->redo($todo);
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->json('data');
+        $todo = $this->getTodo($todo);
 
         $this->assertSame('Test description', $todo['description']);
+
+        $this->undo($todo);
+
+        $this->undo($todo);
+
+        $this->assertTodoMissing($todo);
     }
 
     #[Test]
@@ -56,32 +63,31 @@ class TodoTest extends TestCase
 
         $this->actingAs($user);
 
-        $todo = $this->postJson(route('todos.store'), [
-            'title' => 'Test Todo',
-        ])->json('data');
+        $todo = $this->createTodo('Test Todo');
 
-        $this->patchJson(route('todos.update-assignee', ['todo' => $todo['id']]), [
-            'assignee_id' => $assignee->id,
-        ]);
+        $this->updateAssignee($todo, $assignee);
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->json('data');
+        $todo = $this->getTodo($todo);
 
         $this->assertSame($assignee->id, $todo['assignee_id']);
 
-        $this->patchJson(route('todos.undo', ['todoId' => $todo['id']]));
+        $this->undo($todo);
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->json('data');
+        $todo = $this->getTodo($todo);
 
         $this->assertNull($todo['assignee_id']);
 
-        $this->patchJson(route('todos.redo', ['todoId' => $todo['id']]));
+        $this->redo($todo);
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->json('data');
+        $todo = $this->getTodo($todo);
 
         $this->assertSame($assignee->id, $todo['assignee_id']);
+
+        $this->undo($todo);
+
+        $this->undo($todo);
+
+        $this->assertTodoMissing($todo);
     }
 
     #[Test]
@@ -91,26 +97,67 @@ class TodoTest extends TestCase
 
         $this->actingAs($user);
 
-        $todo = $this->postJson(route('todos.store'), [
-            'title' => 'Test Todo',
-        ])->json('data');
+        $todo = $this->createTodo('Test Todo');
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->json('data');
+        $todo = $this->getTodo($todo);
 
         $this->assertSame('Test Todo', $todo['title']);
 
-        $this->patchJson(route('todos.undo', ['todoId' => $todo['id']]));
+        $this->undo($todo);
 
-        $this->getJson(route('todos.show', ['todo' => $todo['id']]))
-            ->assertStatus(Response::HTTP_NOT_FOUND);
+        $this->assertTodoMissing($todo);
 
-        $this->patchJson(route('todos.redo', ['todoId' => $todo['id']]));
+        $this->redo($todo);
 
-        $todo = $this->getJson(route('todos.show', ['todo' => $todo['id']]))
+        $todo = $this->getTodo($todo);
+
+        $this->assertSame('Test Todo', $todo['title']);
+    }
+
+    private function updateDescription(array $todo, string $description): void
+    {
+        $this->patchJson(route('todos.update-description', ['todo' => $todo['id']]), [
+            'description' => 'Test description',
+        ]);
+    }
+
+    private function getTodo(array $todo): array
+    {
+        return $this->getJson(route('todos.show', ['todo' => $todo['id']]))
             ->assertStatus(Response::HTTP_OK)
             ->json('data');
+    }
 
-        $this->assertSame('Test Todo', $todo['title']);
+    private function undo(array $todo): void
+    {
+        $this->patchJson(route('todos.undo', ['todoId' => $todo['id']]))
+            ->assertStatus(Response::HTTP_OK);
+    }
+
+    private function redo(array $todo): void
+    {
+        $this->patchJson(route('todos.redo', ['todoId' => $todo['id']]));
+    }
+
+    private function updateAssignee(array $todo, User $assignee): void
+    {
+        $this->patchJson(route('todos.update-assignee', ['todo' => $todo['id']]), [
+            'assignee_id' => $assignee->id,
+        ]);
+    }
+
+    private function createTodo(string $title): array
+    {
+        return $this->postJson(route('todos.store'), [
+            'title' => $title,
+        ])
+            ->assertStatus(Response::HTTP_CREATED)
+            ->json('data');
+    }
+
+    private function assertTodoMissing(array $todo): void
+    {
+        $this->getJson(route('todos.show', ['todo' => $todo['id']]))
+            ->assertStatus(Response::HTTP_NOT_FOUND);
     }
 }
